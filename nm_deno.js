47df10a4fcd13b1781c1080bb20a98212865fe15
guest271314 <guest271314@gmail.com>
2023-10-14 23:37:06 -0700

    Update nm_deno.js
    Formatting

--- nm_deno.js
+++ nm_deno.js
@@ -5,38 +5,37 @@
 // https://github.com/denoland/deno/discussions/17236#discussioncomment-4566134
 // https://github.com/saghul/txiki.js/blob/master/src/js/core/tjs/eval-stdin.js
 async function readFullAsync(length) {
-    const buffer = new Uint8Array(65536);
-    const data = [];
-    let n = null;
-    while (data.length < length && (n = await Deno.stdin.read(buffer))) {
-      data.push(...buffer.subarray(0, n));
-    }
-    return new Uint8Array(data);
+  const buffer = new Uint8Array(65536);
+  const data = [];
+  let n = null;
+  while (data.length < length && (n = await Deno.stdin.read(buffer))) {
+    data.push(...buffer.subarray(0, n));
   }
-  
-  async function getMessage() {
-    const header = new Uint32Array(1);
-    await Deno.stdin.read(new Uint8Array(header.buffer));
-    return readFullAsync(header[0]);
+  return new Uint8Array(data);
+}
+
+async function getMessage() {
+  const header = new Uint32Array(1);
+  await Deno.stdin.read(new Uint8Array(header.buffer));
+  return readFullAsync(header[0]);
+}
+
+async function sendMessage(message) {
+  const header = new Uint32Array([message.length]);
+  await Deno.stdout.write(new Uint8Array(header.buffer));
+  await Deno.stdout.write(message);
+}
+
+async function main() {
+  while (true) {
+    const message = await getMessage();
+    await sendMessage(message);
+    gc();
   }
-  
-  async function sendMessage(message) {
-    const header = new Uint32Array([message.length]);
-    await Deno.stdout.write(new Uint8Array(header.buffer));
-    await Deno.stdout.write(message);
-  }
-  
-  async function main() {
-    while (true) {
-      const message = await getMessage();
-      await sendMessage(message);
-      gc();
-    }
-  }
-  
-  try {
-    main();
-  } catch (e) {
-    Deno.exit();
-  }
-  
+}
+
+try {
+  main();
+} catch (e) {
+  Deno.exit();
+}
