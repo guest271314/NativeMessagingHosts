ab2ae124ae8b602717343499258884b28a07f92e
guest271314 <guest271314@gmail.com>
2023-10-14 23:26:10 -0700

    Update nm_nodejs.mjs
    Write to stdout asynchronously

--- nm_nodejs.mjs
+++ nm_nodejs.mjs
@@ -1,50 +1,48 @@
-#!/usr/bin/env -S ./node --max-old-space-size=6 --jitless --expose-gc --v8-pool-size=1
+#!/usr/bin/env -S ./node --jitless --expose-gc --v8-pool-size=1
 // Node.js Native Messaging host
 // guest271314, 10-9-2022
-import {readSync} from 'node:fs';
-// Node.js Native Messaging host constantly increases RSS during usage
-// https://github.com/nodejs/node/issues/43654
+import { open } from "node:fs/promises";
 process.env.UV_THREADPOOL_SIZE = 1;
-// Process greater than 65535 length input
-// https://github.com/nodejs/node/issues/6456
-// https://github.com/nodejs/node/issues/11568#issuecomment-282765300
-process.stdout._handle.setBlocking(true);
+
 // https://github.com/denoland/deno/discussions/17236#discussioncomment-4566134
-function readFullSync(fd, buf) {
-  let offset = 0;
-  while (offset < buf.byteLength) {
-    offset += readSync(fd, buf, { offset });
+// https://github.com/saghul/txiki.js/blob/master/src/js/core/tjs/eval-stdin.js
+async function readFullAsync(length, buffer = new Uint8Array(65536)) {
+  const data = [];
+  while (data.length < length) {
+    const input = await open("/dev/stdin");
+    let { bytesRead } = await input.read({
+      buffer
+    });
+    await input.close();
+    if (bytesRead === 0) {
+      break;
+    }
+    data.push(...buffer.subarray(0, bytesRead));  
   }
-  return buf;
+  return new Uint8Array(data);
 }
 
-function getMessage() {
+async function getMessage() {
   const header = new Uint32Array(1);
-  readFullSync(0, header);
-  const content = new Uint8Array(header[0]);
-  readFullSync(0, content);
+  await readFullAsync(1, header);
+  const content = await readFullAsync(header[0]);
   return content;
 }
 
-function sendMessage(json) {
-  let header = Uint32Array.from({
-    length: 4,
-  }, (_,index)=>(json.length >> (index * 8)) & 0xff);
-  let output = new Uint8Array(header.length + json.length);
-  output.set(header, 0);
-  output.set(json, 4);
-  process.stdout.write(output);
-  // Mitigate RSS increasing expotentially for multiple messages
-  // between client and host during same connectNative() connection
-  header = output = null;
+async function sendMessage(message) {
+  const header = new Uint32Array([message.length]);
+  const stdout = await open("/proc/self/fd/1", "w");
+  await stdout.write(header);
+  await stdout.write(message);
+  await stdout.close();
   global.gc();
 }
 
-function main() {
+async function main() {
   while (true) {
     try {
-      const message = getMessage();
-      sendMessage(message);
+      const message = await getMessage();
+      await sendMessage(message);
     } catch (e) {
       process.exit();
     }
