cdb57f59eb37d8acb847f8e9de8a90f849edbcb5
guest271314 <guest271314@gmail.com>
2024-07-21 23:09:49 -0700

    Include Bash and QuickJS scripts in nm_d8.js

--- nm_d8.js
+++ nm_d8.js
@@ -6,39 +6,24 @@ function encodeMessage(str) {
   return new Uint8Array([...str].map((s) => s.codePointAt()));
 }
 
-function getMessage(pid) {
+function getMessage(pid, [command, argv]) {
   try {
-    // readline() doesn't read past the Uint32Array equivalent message length
-    // V8 authors are not interested in reading STDIN to an ArrayBuffer in d8
-    // https://groups.google.com/g/v8-users/c/NsnStT6bx3Y/m/Yr_Z1FwgAQAJ
-    // Use dd https://pubs.opengroup.org/onlinepubs/9699919799/utilities/dd.html
-    // or GNU Coreutils head https://www.gnu.org/software/coreutils/manual/html_node/head-invocation.html
-    // # length=$(head -q -z --bytes=4 /proc/${pid}/fd/0 | od -An -td4 -)
-    // # message=$(head -q -z --bytes=$((length)) /proc/${pid}/fd/0)
-    // to get message length and return message from Bash script
-    const stdin = (os.system("bash", [
-      "-c",
-      `
-  length=$(dd iflag=fullblock oflag=nocache conv=notrunc,fdatasync bs=4 count=1 if=/proc/${pid}/fd/0 | od -An -td4 -)
-  message=$(dd iflag=fullblock oflag=nocache conv=notrunc,fdatasync bs=$((length)) count=1 if=/proc/${pid}/fd/0)
-  echo "$message"`,
-    ])).trim();
+    const stdin = (os.system(command, argv)).trim();
     if (stdin != undefined && stdin != null && stdin.length) {
-      const message = encodeMessage(stdin);
-      // https://stackoverflow.com/a/58288413
-      // const header = new Uint32Array([
-      //  ((uint32) =>
-      //    (uint32[3] << 24) |
-      //    (uint32[2] << 16) |
-      //    (uint32[1] << 8) |
-      //    (uint32[0]))(Array.from({
-      //    length: 4,
-      //    }, (_, index) => (message.length >> (index * 8)) & 0xff)),
-      // ]);
-      // writeFile("messageLength.txt", encodeMessage(JSON.stringify(header)));
-      return encodeMessage(stdin);
+      const cmd = command.split(/[/-]/).pop();
+      if (cmd === "bash") {
+        const message = encodeMessage(stdin);
+        return message;
+      }
+      if (cmd === "qjs") {
+        const data = encodeMessage(stdin);
+        const view = new DataView(data.subarray(0, 4).buffer);
+        const length = view.getUint32(0, true);
+        // sendMessage(encodeMessage(JSON.stringify({ length })));
+        return data.subarray(4, 4 + length);
+      }
     } else {
-      return;
+      quit(1);
     }
   } catch (e) {
     writeFile("getMessageError.txt", encodeMessage(e.message));
@@ -53,17 +38,77 @@ function sendMessage(message) {
 
 function main() {
   // Get PID of current process
-  const pid = (os.system("pgrep", ["-n", "-f", os.d8Path])).replace(/\D+/g, "");
+  const pid = (os.system("pgrep", ["-n", "-f", os.d8Path])).replace(/\D+/g, "")
+    .trim();
   // Get PPID of current process
   const ppid = os.system("ps", ["-o", "ppid=", "-p", JSON.parse(pid)]);
+  // readline() doesn't read past the Uint32Array equivalent message length
+  // V8 authors are not interested in reading STDIN to an ArrayBuffer in d8
+  // https://groups.google.com/g/v8-users/c/NsnStT6bx3Y/m/Yr_Z1FwgAQAJ
+  // Use dd https://pubs.opengroup.org/onlinepubs/9699919799/utilities/dd.html
+  // or GNU Coreutils head https://www.gnu.org/software/coreutils/manual/html_node/head-invocation.html
+  // or QuickJS https://bellard.org/quickjs/
+  const bash = ["bash", [
+    "--posix",
+    "-c",
+    `
+length=$(dd iflag=fullblock oflag=nocache conv=notrunc,fdatasync bs=4 count=1 if=/proc/${pid}/fd/0 | od -An -td4 -)
+message=$(dd iflag=fullblock oflag=nocache conv=notrunc,fdatasync bs=$((length)) count=1 if=/proc/${pid}/fd/0)
+printf "$message"
+`,
+  ]];
+  const qjs = ["/home/xubuntu/bin/qjs", [
+    "--std",
+    "-m",
+    "-e",
+    `const path = "/proc/${pid}/fd/0";
+  try {
+    const size = new Uint32Array(1);
+    const err = { errno: 0 };
+    const pipe = std.open(
+      path,
+      "rb",
+      err,
+    );
+    if (err.errno !== 0) {
+      throw std.strerror(err.errno);
+    }
+    pipe.read(size.buffer, 0, 4);
+    // writeFile("len.txt", size);
+    // {error: 'writeFile is not defined'
+    const output = new Uint8Array(size[0]);
+    pipe.read(output.buffer, 0, size[0]);
+    const res = new Uint8Array([...new Uint8Array(size.buffer),...output]);
+    std.out.write(res.buffer, 0, res.length);
+    std.out.flush();
+    std.exit(0);
+  } catch (e) {
+    const json = JSON.stringify({error:e.message});
+    std.out.write(Uint32Array.of(json.length).buffer, 0, 4);
+    std.out.puts(json);
+    std.out.flush();
+    std.exit(0);
+  }
+`,
+  ]];
   while (true) {
     // Terminate current process when chrome processes close
     if (!(os.system("pgrep", ["-P", JSON.parse(ppid)]))) {
       break;
     }
-    const message = getMessage(pid);
+    const message = getMessage(
+      pid,
+      qjs,
+    );
     if (message) {
       sendMessage(message);
+      if (
+        // Handle error from qjs
+        String.fromCodePoint.apply(null, [...message.subarray(1, 8)]) ===
+          `"error"` // JSON
+      ) {
+        break;
+      }
     }
   }
 }
