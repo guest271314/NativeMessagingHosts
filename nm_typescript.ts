9f6a15f91a8b3615f9bbc3e52af63441341c3734
guest271314 <guest271314@gmail.com>
2025-10-05 20:40:22 +0000

    Just pass Uint32Array to Blob(), use 0 and 1 for Bun.file()

--- nm_typescript.ts
+++ nm_typescript.ts
@@ -1,31 +1,13 @@
 /*
-  #!/usr/bin/env -S /home/user/deno -A
-  #!/usr/bin/env -S /home/user/bun run
-  #!/usr/bin/env -S /home/user/node --experimental-transform-types
+  #!/usr/bin/env -S /home/user/deno
+  #!/usr/bin/env -S /home/user/bun
+  #!/usr/bin/env -S /home/user/node
 
   TypeScript Native Messaging host
   guest271314, 7-28-2024
 */
 
-/*
-declare let readable: NodeJS.ReadStream & {
-    fd: 0;
-} | ReadableStream<Uint8Array>, writable: WritableStream<Uint8Array>, exit: () => void;
-declare function encodeMessage(message: object): Uint8Array;
-declare function getMessage(): AsyncGenerator<Uint8Array>;
-declare function sendMessage(message: Uint8Array): Promise<void>;
-export { encodeMessage, exit, getMessage, readable, sendMessage, writable, };
-*/
-
 // Source JavaScript: https://github.com/guest271314/NativeMessagingHosts/blob/main/nm_host.js
-//
-// Convert JavaScript to TypeScript, no obvious equivalent with tsc
-// https://www.codeconvert.ai/javascript-to-typescript-converter
-//
-// Resizable ArrayBuffer supported by tsc Version 5.7.0-dev.20241019
-/**
-* /// <reference types="https://raw.githubusercontent.com/microsoft/TypeScript/2ac4cb78d6930302eb0a55d07f154a2b0597ae32/src/lib/es2024.arraybuffer.d.ts" />
-*/
 
 import process from "node:process";
 const runtime: string = navigator.userAgent;
@@ -38,11 +20,8 @@ let readable: NodeJS.ReadStream & { fd: 0 } | ReadableStream<Uint8Array>,
   exit: () => void = () => {};
 
 if (runtime.startsWith("Deno")) {
-  // @ts-ignore Deno
   ({ readable } = Deno.stdin);
-  // @ts-ignore Deno
   ({ writable } = Deno.stdout);
-  // @ts-ignore Deno
   ({ exit } = Deno);
 }
 
@@ -58,11 +37,12 @@ if (runtime.startsWith("Node")) {
 
 if (runtime.startsWith("Bun")) {
   // @ts-ignore Bun
-  readable = Bun.file("/dev/stdin").stream();
+  readable = Bun.file(0).stream();
   writable = new WritableStream<Uint8Array>({
     async write(value) {
       // @ts-ignore Bun
-      await Bun.write(Bun.stdout, value);
+      Bun.file(1)
+      .writer().write(value);
     },
   }, new CountQueuingStrategy({ highWaterMark: Infinity }));
   ({ exit } = process);
@@ -100,7 +80,7 @@ async function* getMessage(): AsyncGenerator<Uint8Array> {
 
 async function sendMessage(message: Uint8Array): Promise<void> {
   await new Blob([
-    new Uint8Array(new Uint32Array([message.length]).buffer),
+    new Uint32Array([message.length]),
     message,
   ])
     .stream()
